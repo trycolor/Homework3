e55ff64bfac18e9beab708fcbb9f93b11e2dcaaa
Oleg <hillott@yandex.ru>
2022-12-04 22:48:56 +0300

    первое задание

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int one;
+        one = 1;
+        byte two;
+        two = 2;
+        short three;
+        three = 3;
+        long four;
+        four = 4;
+        float five;
+        five = 5.0f;
+        double six;
+        six = 6.0;
+        System.out.println("Значение пременной one c типом данных int равно " + one);
+        System.out.println("Значение пременной two c типом данных byte равно " + two);
+        System.out.println("Значение пременной three c типом данных short равно " + three);
+        System.out.println("Значение пременной four c типом данных long равно " + four);
+        System.out.println("Значение пременной five c типом данных float равно " + five);
+        System.out.println("Значение пременной six c типом данных double равно " + six);
+    }
+}
