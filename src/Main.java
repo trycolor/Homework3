b87f7607f243148a3754674cc1a64dbb317d496a
Oleg <hillott@yandex.ru>
2022-12-04 23:28:35 +0300

    второе задание

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,21 @@ public class Main {
         System.out.println("Значение пременной four c типом данных long равно " + four);
         System.out.println("Значение пременной five c типом данных float равно " + five);
         System.out.println("Значение пременной six c типом данных double равно " + six);
+
+        //задание 2
+        double a = 27.12;
+        long b = 987678965549L;
+        byte c = 2;
+        short d = 786;
+        boolean e = false;
+        short f = 569;
+        short g = -159;
+        short j = 27897;
+        byte i = 67;
+        int h = 74674;
+        char k = 56;
+        float r = 4.7f;
+
+
     }
 }
